d12072d5895c50e47ab9116b907f7a54bef8ab11
huniieee <sehoon981014@gmail.com>
2025-08-13 18:23:22 +0900

    chore(payment) domain/payment 간단 수정
    1. client 어노테이션 component로 수정
    
    #ref4

--- src/main/java/app/domain/payment/client/InternalOrderClient.java
+++ src/main/java/app/domain/payment/client/InternalOrderClient.java
@@ -1,10 +1,13 @@
 package app.domain.payment.client;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,7 +15,7 @@ import org.springframework.beans.factory.annotation.Value;
 import app.domain.payment.model.dto.request.OrderInfo;
 import app.global.apiPayload.ApiResponse;
 
-@Service
+@Component
 public class InternalOrderClient {
 	private final RestTemplate restTemplate;
 
@@ -38,10 +41,12 @@ public class InternalOrderClient {
 
 	public ApiResponse<String> updateOrderStatus(UUID orderId, String orderStatus) {
 		String url = orderServiceUrl+"/internal/order/"+orderId+"/status";
+
+		HttpEntity<String> requestEntity = new HttpEntity<>(orderStatus);
 		ResponseEntity<ApiResponse<String>> response = restTemplate.exchange(
 			url,
 			HttpMethod.POST,
-			null,
+			requestEntity,
 			new ParameterizedTypeReference<ApiResponse<String>>() {}
 		);
 		return response.getBody();
