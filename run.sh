0bc76665385d8454ed4d3003e686d5b5c7dac007
huniieee <sehoon981014@gmail.com>
2025-08-30 15:29:35 +0900

    fix(payment) 로컬에서 이미지 실행되도록 수정
    common uitl 1.0.1 -> 1.0.2  수정
    run.sh 수정

--- run.sh
+++ run.sh
@@ -13,8 +13,14 @@ docker run --name payment \
     -e DB_URL=jdbc:postgresql://postgres:5432/order_platform \
     -e DB_USERNAME=bonun \
     -e DB_PASSWORD=password \
+    -e REDIS_HOST=redis \
+    -e REDIS_PORT=6379 \
+    -e REDIS_PASSWORD=password \
     -e OAUTH_JWKS_URI=http://host.docker.internal:8083/oauth/jwks \
     -e AUTH_INTERNAL_AUDIENCE=internal-services \
+    -e TOSS_CLIENT_KEY=test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm \
+    -e TOSS_SECRET_KEY=test_gsk_docs_OaPz8L5KdmQXkzRz3y47BMw6 \
+    -e TOSS_URL=https://api.tosspayments.com/v1/payments \
     -d order-platform-msa-payment
 
 
